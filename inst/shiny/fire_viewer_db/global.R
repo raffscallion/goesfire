542777ebede16b73784cf11d0916a39f9324f88c
sillson <stuartillson@gmail.com>
2019-10-11 15:32:36 -0700

    ui and odbc

--- inst/shiny/fire_viewer_db/global.R
+++ inst/shiny/fire_viewer_db/global.R
@@ -2,11 +2,13 @@
 library(shiny)
 library(DBI)
 library(pool)
+library(odbc)
 library(dplyr)
 library(dbplyr)
 library(leaflet)
 library(leaflet.extras)
 library(plotly)
+library(shinycssloaders)
 library(lubridate)
 library(imputeTS)
 library(htmltools)
@@ -24,9 +26,15 @@ Sys.setenv(TZ = "UTC")
 # Connect to the db
 if (.Platform$OS.type == "windows") {
   driver <- "PostgreSQL Unicode(x64)"
+} else if (.Platform$OS.type == "unix") {
+  # When using ODBC, ensure you have drivers installed locally
+  # Install the unixODBC library
+  # apt-get install unixodbc unixodbc-dev odbc-postgresql
+  driver <- "PostgreSQL Unicode"
 } else {
   driver <- "WHATEVER DRIVER YOU'RE USING FOR POSTGRES"
 }
+
 pool <- dbPool(odbc::odbc(), Driver = driver, Database = "airfire", UID = "user",
                PWD = "pwd",
                Server = "postgresql.cx7b6gvf3kxs.us-west-2.rds.amazonaws.com",
@@ -45,7 +53,6 @@ date_min <- as.Date(date_range[[1]])
 
 palette <- leaflet::colorFactor("viridis", domain = c(10, 11, 12, 13, 14, 15, 30, 31,
                                                       32, 33, 34, 35))
-
 source("model.R")
 
 # Cleanup after close
